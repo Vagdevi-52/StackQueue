b11fa01e638ecc4e56798ab1b51a32eb08274902
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-02 08:53:51 -0700

    :Added:create a stack

---
+++ src/stackandqueue/StackMain.java
@@ -0,0 +1,19 @@
+package stackandqueue;
+
+public class StackMain
+{
+	public static void main(String[] args)
+	{
+
+		System.out.println("Welcome to Stack  Program");
+		Stack stack = new Stack();
+		
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		
+		stack.print();
+
+	}
+
+}
