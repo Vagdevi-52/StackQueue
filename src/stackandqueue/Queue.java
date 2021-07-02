89024382f3b6c7a9fc022af448ede942f6ffb161
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-02 10:08:32 -0700

    Added:Ability to create a queue

---
+++ src/stackandqueue/Queue.java
@@ -0,0 +1,42 @@
+package stackandqueue;
+
+public class Queue
+{
+	Node front;
+	Node rear;
+
+	int length;
+
+	class Node{
+		int data;
+		Node next;
+		public Node(int data) {
+
+			this.data = data;
+			this.next = null;
+		}
+
+	}
+	public void enQueue(int data) {
+		if (front == null) {
+			rear = new Node(data);
+			front = rear;
+		}
+		else {
+			rear.next = new Node(data);
+			rear = rear.next;
+		}
+
+		length ++;
+
+	}
+	public void print() {
+		Node temp = front;
+		while(temp != null) {
+			System.out.print(temp.data + "-> ");
+			temp = temp.next;
+		}
+
+	}
+
+}
